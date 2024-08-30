ffe0fc85fdf24d1a00e455092942c94544a07e9f
Keerthana J <jkeerthana2004@gmail.com>
2024-08-30 07:08:28 +0530

    navbar corrected

--- portfolio.js
+++ portfolio.js
@@ -13,3 +13,10 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
